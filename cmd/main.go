d9040e87401e7b0f8a430c025f41ade544e05a3e
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-01 23:48:37 +0200

    basic run flow

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Petroviiic/GoTorrent/internal/bencode"
+)
 
 func main() {
-	fmt.Println("cao")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run ./... <path_to_torrent_file>")
+		os.Exit(1)
+	}
+
+	path := os.Args[1]
+
+	fmt.Println(path)
+
+	err := bencode.Decode(path)
+
+	if err != nil {
+		fmt.Printf("Fatal: error %v", err)
+		os.Exit(1)
+	}
+
 }
