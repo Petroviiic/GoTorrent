59b58abd9dcb7f629fc13d47e581c4755803fa66
MisterDodik <drazenpetrovic56@gmail.com>
2026-06-06 01:20:36 +0200

    Treat bencode strings as []byte and update parsing
    Change InfoDict.Pieces from string to []byte and update decoding to preserve raw byte slices. DecodeString now returns []byte, DecodeDictionary converts keys to string, and parseDictionaryData converts byte-slice values (announce, comment, created_by, info.name) into strings while properly handling announce-list nested byte slices into [][]string. Also assign the decoded top-level map to mainMap, replace a debug fmt.Println with fmt.Printf("%+v\n", data), and comment out some debug/unused branches (integer/list handlers) and prints.

--- internal/bencode/decoder.go
+++ internal/bencode/decoder.go
@@ -19,7 +19,7 @@ type InfoDict struct {
 	Length      int    `bencode:"length"`
 	Name        string `bencode:"name"`
 	PieceLength int    `bencode:"piece length"`
-	Pieces      string `bencode:"pieces"`
+	Pieces      []byte `bencode:"pieces"`
 }
 
 func LoadAndDecode(path string) error {
@@ -36,7 +36,7 @@ func LoadAndDecode(path string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(data)
+	fmt.Printf("%+v\n", data)
 	return nil
 }
 
@@ -45,30 +45,30 @@ func (d *Decoder) Decode(buffer []byte, index int) (any, error) {
 	mainMap := map[any]any{}
 	for i := index; i < len(buffer); {
 		switch b := buffer[i]; {
-		case b == 'i':
-			res, newIndex, err := d.Decoders[b](i)
+		// case b == 'i':
+		// 	res, newIndex, err := d.Decoders[b](i)
 
-			if err != nil {
-				//ovdje vjv treba return, jer ce biti infinity loop ako se desi greska
-				break
-			}
+		// 	if err != nil {
+		// 		//ovdje vjv treba return, jer ce biti infinity loop ako se desi greska
+		// 		break
+		// 	}
 
-			i = newIndex
-			fmt.Println(res)
+		// 	i = newIndex
+		// 	fmt.Println(res)
 
-			if res == 497360 {
-				fmt.Println("EVO OME SPASAVAM TE OD SPAMA PLIZ SE SAUSTAVI")
-				return nil, nil
-			}
-		case b == 'l':
-			res, newIndex, err := d.Decoders[b](i)
+		// 	if res == 497360 {
+		// 		fmt.Println("EVO OME SPASAVAM TE OD SPAMA PLIZ SE SAUSTAVI")
+		// 		return nil, nil
+		// 	}
+		// case b == 'l':
+		// 	res, newIndex, err := d.Decoders[b](i)
 
-			if err != nil {
-				break
-			}
+		// 	if err != nil {
+		// 		break
+		// 	}
 
-			i = newIndex
-			fmt.Println(res)
+		// 	i = newIndex
+		// 	fmt.Println(res)
 		case b == 'd':
 			res, newIndex, err := d.Decoders[b](i)
 
@@ -77,17 +77,18 @@ func (d *Decoder) Decode(buffer []byte, index int) (any, error) {
 			}
 
 			i = newIndex
-			fmt.Println(res)
-		case b >= '0' && b <= '9':
-			res, newIndex, err := d.Decoders[b](i)
+			// fmt.Println(res)
+			mainMap = res.(map[any]any)
+			// case b >= '0' && b <= '9':
+			// 	res, newIndex, err := d.Decoders[b](i)
 
-			if err != nil {
-				break
-			}
+			// 	if err != nil {
+			// 		break
+			// 	}
 
-			i = newIndex
+			// 	i = newIndex
 
-			fmt.Println(res)
+			// 	fmt.Println(res)
 		}
 
 		//i++
@@ -99,16 +100,24 @@ func parseDictionaryData(mainMap map[any]any) *TorrentFile {
 	torrentData := &TorrentFile{}
 
 	if info, exists := mainMap["announce"]; exists {
-		torrentData.Announce = info.(string)
+		torrentData.Announce = string(info.([]byte))
 	}
 	if announce_list, exists := mainMap["announce-list"]; exists {
-		torrentData.AnnounceList = announce_list.([][]string)
+
+		for _, outer := range announce_list.([]interface{}) {
+			items := []string{}
+			for _, inner := range outer.([]interface{}) {
+				items = append(items, string(inner.([]byte)))
+			}
+
+			torrentData.AnnounceList = append(torrentData.AnnounceList, items)
+		}
 	}
 	if comment, exists := mainMap["comment"]; exists {
-		torrentData.Comment = comment.(string)
+		torrentData.Comment = string(comment.([]byte))
 	}
 	if created_by, exists := mainMap["created by"]; exists {
-		torrentData.CreatedBy = created_by.(string)
+		torrentData.CreatedBy = string(created_by.([]byte))
 	}
 	if creation_date, exists := mainMap["creation date"]; exists {
 		torrentData.CreationDate = creation_date.(int)
@@ -117,7 +126,7 @@ func parseDictionaryData(mainMap map[any]any) *TorrentFile {
 	if infoDict, exists := mainMap["info"]; exists {
 		inner := infoDict.(map[any]any)
 		if name, ok := inner["name"]; ok {
-			torrentData.Info.Name = name.(string)
+			torrentData.Info.Name = string(name.([]byte))
 		}
 		if length, ok := inner["length"]; ok {
 			torrentData.Info.Length = length.(int)
@@ -125,7 +134,9 @@ func parseDictionaryData(mainMap map[any]any) *TorrentFile {
 		if pieceLength, ok := inner["piece length"]; ok {
 			torrentData.Info.PieceLength = pieceLength.(int)
 		}
-		//pieces
+		if pieces, ok := inner["pieces"]; ok {
+			torrentData.Info.Pieces = pieces.([]byte)
+		}
 	}
 
 	return torrentData
@@ -159,7 +170,7 @@ func (d *Decoder) DecodeString(index int) (any, int, error) {
 		return "", -1, err
 	}
 
-	return string(d.Buffer[end+1 : end+1+num]), end + num + 1, nil
+	return d.Buffer[end+1 : end+1+num], end + num + 1, nil
 }
 
 func (d *Decoder) DecodeList(index int) (any, int, error) {
@@ -195,12 +206,12 @@ func (d *Decoder) DecodeDictionary(index int) (any, int, error) {
 	for i := index + 1; i < len(d.Buffer); {
 		b := (d.Buffer)[i]
 		item, newIndex, err := d.Decoders[b](i)
-
 		if err != nil {
 			return nil, -1, err
 		}
 
 		if isKey {
+			item = string(item.([]byte))
 			res[item] = false
 			lastKey = item
 			isKey = false
