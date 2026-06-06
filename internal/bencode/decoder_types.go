1a1a882104f4df7cda9c614a41ebb4df44d0fd49
MisterDodik <drazenpetrovic56@gmail.com>
2026-06-07 01:06:18 +0200

    udpate name decoder_type.go

---
+++ internal/bencode/decoder_types.go
@@ -0,0 +1,23 @@
+package bencode
+
+type Decoder struct {
+	Buffer   []byte
+	Decoders map[byte]func(int) (any, int, error) // Funkcije sad primaju samo indeks!
+}
+
+func NewDecoder(buffer []byte) *Decoder {
+	decoder := &Decoder{
+		Buffer:   buffer,
+		Decoders: make(map[byte]func(int) (any, int, error)),
+	}
+
+	decoder.Decoders['i'] = decoder.DecodeInt
+	decoder.Decoders['l'] = decoder.DecodeList
+	decoder.Decoders['d'] = decoder.DecodeDictionary
+	decoder.Decoders['e'] = decoder.DecodeEnd
+	for c := '0'; c <= '9'; c++ {
+		decoder.Decoders[byte(c)] = decoder.DecodeString
+	}
+
+	return decoder
+}
