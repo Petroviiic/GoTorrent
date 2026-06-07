d215e55bc43fbaf812230d2125f02cb1b423acac
MisterDodik <drazenpetrovic56@gmail.com>
2026-06-08 01:38:23 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -15,28 +15,14 @@ func main() {
 
 	path := os.Args[1]
 
-	fmt.Println(path)
+	// fmt.Println(path)
 
-	torrentFile, err := bencode.LoadAndDecode(path)
+	torrentFile, infoDictEncode, err := bencode.LoadAndDecode(path)
 	if err != nil {
 		fmt.Printf("Fatal: error %v", err)
 		os.Exit(1)
 	}
-	buffer, err := os.ReadFile(path)
-	encoded, err := bencode.Encode(torrentFile)
-	if err != nil {
-		fmt.Printf("Fatal: error %v", err)
-		os.Exit(1)
-	}
+
 	_ = torrentFile
-	_ = buffer
-	_ = encoded
-
-	// 	for i := range encoded {
-	// 		if encoded[i] != buffer[i] {
-	// 			fmt.Println("kurcina")
-	// 		}
-	// 	}
-	// 	// fmt.Println(encoded == buffer)
-	// 	//fmt.Println(buffer)
+	_ = infoDictEncode
 }
