feb88bac0f2b878d433688727e9d1b981f6470ef
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-12 23:29:56 +0200

    Return peers/errors from tracker funcs
    Introduce a Peer struct and change tracker API to return parsed peers and errors instead of logging/printing. GetPeers now returns ([]*Peer, error) and propagates request/decoding errors. decodePeerBody was updated to return ([]*Peer, error), validates presence of the "peers" key, and delegates parsing to decodePeerList. decodePeerList(peers []byte) is added as a stub to implement compact peer parsing later.

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -5,21 +5,41 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/Petroviiic/GoTorrent/internal/bencode"
 )
 
-func GetPeers(torrentData *bencode.TorrentFile, infoHash, peerID []byte) {
+type Peer struct {
+	ipAdress string
+	port     string
+}
+
+func GetPeers(torrentData *bencode.TorrentFile, infoHash, peerID []byte) ([]*Peer, error) {
 	req, err := http.NewRequest("GET", torrentData.Announce, nil)
 	if err != nil {
-		log.Print(err)
-		return
+		return nil, err
 	}
 
 	params := req.URL.Query()
 
-	left := strconv.Itoa(torrentData.Info.Length)
+	left := ""
+	if len(torrentData.Info.Files) == 0 {
+		left = fmt.Sprintf("%d", torrentData.Info.Length)
+	} else {
+		leftNum := 0
+		for _, file := range torrentData.Info.Files {
+			leftNum += file.Length
+		}
+
+		left = fmt.Sprintf("%d", leftNum)
+	}
+
+	if left == "" {
+		return nil, fmt.Errorf("something went wrong. 'left' is empty")
+	}
+	fmt.Println(left)
+	return nil, nil
+
 	params.Add("info_hash", string(infoHash))
 	params.Add("peer_id", string(peerID))
 	params.Add("port", "6881")
@@ -39,21 +59,34 @@ func GetPeers(torrentData *bencode.TorrentFile, infoHash, peerID []byte) {
 	body, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(body))
 
-	decodePeerBody(body)
+	peers, err := decodePeerBody(body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return peers, nil
 }
 
-func decodePeerBody(body []byte) {
+func decodePeerBody(body []byte) ([]*Peer, error) {
 	decoder := bencode.NewDecoder(body)
 	res, err := decoder.Decode(decoder.Buffer, 0)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return nil, err
 	}
-	fmt.Println(res)
-	fmt.Println(string(res["peers"].([]byte)))
+
+	if _, ok := res["peers"]; !ok {
+		return nil, fmt.Errorf("something went wrong. peers not present in response body")
+	}
+
+	peers := decodePeerList(res["peers"].([]byte))
+
+	return peers, nil
 }
 
-func decodePeerList() {
+func decodePeerList(peers []byte) []*Peer {
+	res := []*Peer{}
 
+	return res
 }
