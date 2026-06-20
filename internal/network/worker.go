1e74a9e5432a0dfe5ab41e83f45e6a7240cc9158
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-20 20:51:08 +0200

    Update worker.go

--- internal/network/worker.go
+++ internal/network/worker.go
@@ -1,44 +1,6 @@
 package network
 
-import (
-	"fmt"
-	"net"
-
-	"github.com/Petroviiic/GoTorrent/internal/message"
-	"github.com/Petroviiic/GoTorrent/internal/peer"
-)
-
 type PieceOfWork struct {
 }
 type PieceOfResult struct {
-}
-
-type Worker struct {
-	Conn      net.Conn
-	PeerState *peer.PeerState
-}
-
-func NewWorker(conn net.Conn) *Worker {
-	client := &Worker{
-		Conn:      conn,
-		PeerState: peer.NewPeerState(),
-	}
-	return client
-}
-func (p *Worker) StartWorker() {
-	defer p.Conn.Close()
-
-	for {
-		msg, err := message.Deserialize(p.Conn)
-
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		if msg.ID == message.Bitfield {
-			p.PeerState.Bitfield = msg.Payload
-		}
-		fmt.Println("success", msg)
-	}
 }
