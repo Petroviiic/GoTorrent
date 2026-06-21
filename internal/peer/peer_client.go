d8c57822492014a22d0f66199b7bedc5af9bbadc
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-22 00:22:49 +0200

    Update peer_client.go

--- internal/peer/peer_client.go
+++ internal/peer/peer_client.go
@@ -1,16 +1,20 @@
 package peer
 
-type PeerState struct {
+import "net"
+
+type PeerClient struct {
 	Choked     bool
 	Interested bool
 	Bitfield   []byte
+	Conn       net.Conn
 }
 
-func NewPeerState() *PeerState {
-	client := &PeerState{
+func NewPeerClient(conn net.Conn) *PeerClient {
+	client := &PeerClient{
 		Choked:     true,
 		Interested: false,
 		Bitfield:   nil,
+		Conn:       conn,
 	}
 	return client
 }
