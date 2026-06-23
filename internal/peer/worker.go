3aaa4a1fdc30be075bd766f506068b889f863288
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-23 03:29:22 +0200

    Handle peer messages and change piece types
    Replace string-based piece hashes with byte slices and add Length to PieceOfWork; rename PieceOfResult.Hash to Downloaded ([]byte). Add encoding/binary import and broaden StartWorker to switch on message types: handle Choke/Unchoke/Interested/Not_interested flags, parse Have payload to update pieces using big-endian index, assign Bitfield, and add placeholders for Request/Piece/Cancel. Adjust EOF handling to return from the read loop and add a TODO'ed workChannel line for future piece work consumption.

--- internal/peer/worker.go
+++ internal/peer/worker.go
@@ -1,6 +1,7 @@
 package peer
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"sync"
@@ -9,12 +10,13 @@ import (
 )
 
 type PieceOfWork struct {
-	Index int
-	Hash  string
+	Index  int
+	Hash   []byte
+	Length int
 }
 type PieceOfResult struct {
-	Index int
-	Hash  string
+	Index      int
+	Downloaded []byte
 }
 
 func (p *PeerClient) StartWorker(wg *sync.WaitGroup) {
@@ -30,12 +32,35 @@ func (p *PeerClient) StartWorker(wg *sync.WaitGroup) {
 			if err != io.EOF {
 				fmt.Println("error deserializing message:", err)
 			}
-			continue
+			return
 		}
 
 		fmt.Println("success", msg)
-		if msg.ID == message.Bitfield {
+		switch msg.ID {
+		case message.Choke:
+			p.Choked = true
+		case message.Unchoke:
+			p.Choked = false
+		case message.Interested:
+			p.Interested = true
+		case message.Not_interested:
+			p.Interested = false
+		case message.Have:
+			index := binary.BigEndian.Uint32(msg.Payload[1:])
+			p.UpdatePiece(int(index))
+		case message.Bitfield:
 			p.Bitfield = msg.Payload
+		case message.Request:
+
+		case message.Piece:
+			//dosao piece koji sam requestovao
+		case message.Cancel:
+
+		default:
+			fmt.Println("unknown message type")
 		}
+
+		//pieceWork := <- workChannel
+
 	}
 }
