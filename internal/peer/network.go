c6148c0a75057d562478640cafb6ca5f779462e1
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-06-24 22:01:55 +0200

    Update network.go

--- internal/peer/network.go
+++ internal/peer/network.go
@@ -16,12 +16,12 @@ func ConnectToPeers(peers []*tracker.Peer, infoHash []byte, peerID []byte) []*Pe
 
 	ourHandshake := handshake.NewHandshake([]byte("BitTorrent protocol"), infoHash, peerID)
 	ours := ourHandshake.Serialize()
-	for _, peerInfo := range peers {
+	for i, peerInfo := range peers {
 		address := fmt.Sprintf("%v:%d", peerInfo.IP, peerInfo.Port)
 		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("%d: %v\n", i+1, err)
 			continue
 		}
 
