202d791b2ffddc7a3a255ecb46840a7e94b830e4
MisterDodik <drazenpetrovic56@gmail.com>
2026-07-22 18:42:08 +0200

    Improve tracker announce peer discovery
    Adds a larger built-in list of public HTTP trackers to increase peer discovery coverage. Tracker announce requests now include percent-encoded `info_hash`/`peer_id` via a new helper, request more peers (`numwant=100`), and use a longer timeout (10s) for better reliability with slower trackers.

--- internal/tracker/tracker.go
+++ internal/tracker/tracker.go
@@ -49,6 +49,38 @@ func GetPeers(torrentData *bencode.TorrentFile, infoHash, peerID []byte) ([]*Pee
 			}
 		}
 	}
+	trackerURLs = append(trackerURLs,
+		"http://tracker.opentrackr.org:1337/announce",
+		"http://open.tracker.cl:1337/announce",
+		"http://www.torrentsnipe.info:2701/announce",
+		"http://www.genesis-sp.org:2710/announce",
+		"http://tracker2.dler.org:80/announce",
+		"http://tracker.zhuqiy.dgj055.icu:80/announce",
+		"http://tracker.xiaoduola.xyz:6969/announce",
+		"http://tracker.sbsub.com:2710/announce",
+		"http://tracker.renfei.net:8080/announce",
+		"http://tracker.qu.ax:6969/announce",
+		"http://tracker.mywaifu.best:6969/announce",
+		"http://tracker.lintk.me:2710/announce",
+		"http://tracker.ipv6tracker.org:80/announce",
+		"http://tracker.dler.org:6969/announce",
+		"http://tracker.bt4g.com:2095/announce",
+		"http://tracker.bt-hash.com:80/announce",
+		"http://tracker.bittor.pw:1337/announce",
+		"http://tracker.23794.top:6969/announce",
+		"http://tr.kxmp.cf:80/announce",
+		"http://seeders-paradise.org:80/announce",
+		"http://lucke.fenesisu.moe:6969/announce",
+		"http://buny.uk:6969/announce",
+		"http://bittorrent-tracker.e-n-c-r-y-p-t.net:1337/announce",
+		"http://1337.abcvg.info:80/announce",
+		"http://tracker.zhuqiy.com:80/announce",
+		"http://tracker.waaa.moe:6969/announce",
+		"http://tracker.privateseedbox.xyz:2710/announce",
+		"http://tracker.nexusstream.eu:6969/announce",
+		"http://tracker.dler.com:6969/announce",
+		"http://tracker.dhitechnical.com:6969/announce",
+	)
 	uniquePeers := make(map[string]*Peer)
 	for _, trackerURL := range trackerURLs {
 		newPeers, err := sendRequest(trackerURL, infoHash, peerID, left)
@@ -77,19 +109,26 @@ func sendRequest(trackerURL string, infoHash, peerID []byte, left string) ([]*Pe
 	}
 
 	params := req.URL.Query()
-	params.Add("info_hash", string(infoHash))
-	params.Add("peer_id", string(peerID))
+	params.Add("info_hash", urlEncodeBytes(infoHash))
+	params.Add("peer_id", urlEncodeBytes(peerID))
 	params.Add("port", "6881")
 	params.Add("uploaded", "0")
 	params.Add("downloaded", "0")
 	params.Add("left", left)
 	params.Add("compact", "1")
+	params.Add("numwant", "100")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// encodedInfoHash := urlEncodeBytes(infoHash)
+	// encodedPeerID := urlEncodeBytes(peerID)
+	// req.URL.RawQuery = fmt.Sprintf(
+	// 	"info_hash=%s&peer_id=%s&port=6881&uploaded=0&downloaded=0&left=%s&compact=1&numwant=50&event=started", encodedInfoHash, encodedPeerID, left,
+	// )
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	req.URL.RawQuery = params.Encode()
 	req = req.WithContext(ctx)
 
+	// fmt.Println(req.URL.String())
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Printf("Request failed: %v", err)
@@ -97,13 +136,23 @@ func sendRequest(trackerURL string, infoHash, peerID []byte, left string) ([]*Pe
 	}
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(io.LimitReader(resp.Body, 4*1024*1024))
-
+	// fmt.Printf("Sirovi odgovor od trackera (string): %s\n", string(body))
+	// fmt.Printf("Sirovi odgovor (hex/bytes len): %d\n", len(body))
 	peers, err := decodePeerBody(body)
 	if err != nil {
 		return nil, err
 	}
 
 	return peers, nil
+}
+
+func urlEncodeBytes(b []byte) string {
+	var buf strings.Builder
+	for _, byteVal := range b {
+		buf.WriteByte('%')
+		buf.WriteString(fmt.Sprintf("%02X", byteVal))
+	}
+	return buf.String()
 }
 
 func decodePeerBody(body []byte) ([]*Peer, error) {
