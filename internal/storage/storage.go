b3bfa0b58f9b5c26deef5cdc1e40030e52dd91d8
MisterDodik <drazenpetrovic56@gmail.com>
2026-07-29 18:28:32 +0200

    Add disk piece hash scan in storage
    Implement `ScanDiskForDownloaded` in storage to read data across files in piece-sized chunks and compare SHA-1 hashes against expected piece hashes. The change adds buffered file reading logic with cross-file piece assembly support, plus temporary debug output and a helper test reader method.

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,21 +1,27 @@
 package storage
 
 import (
+	"bufio"
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
 
 type FileInfo struct {
-	Size        int64
-	GlobalStart int64
-	GlobalEnd   int64
+	Size        int64 //file size in bytes
+	GlobalStart int64 //begining of storage (and all files of course) is 0
+	GlobalEnd   int64 //basically GlobalStart+Size
 	Handler     *os.File
 }
 
 type Storage struct {
 	files       []FileInfo
-	pieceLength int64
-	totalSize   int64
+	pieceLength int64 //in bytes
+	totalSize   int64 //all files size
 
 	Buffer chan DownloadedPieceData
 }
@@ -66,6 +72,76 @@ func (s *Storage) Close() {
 			file.Handler.Close()
 		}
 	}
+}
+func (s *Storage) testfunc() {
+	file := s.files[0]
+	reader := bufio.NewReader(file.Handler)
+
+	buffer := make([]byte, s.pieceLength)
+	counter := 0
+	bufferStart := 0
+	for {
+		n, err := reader.Read(buffer[bufferStart:])
+		bufferStart = 0
+		if err != nil {
+			if err.Error() == io.EOF.Error() {
+				fmt.Println("tu sam", n, counter)
+				break
+			}
+			log.Fatalf("error reading file: %s", err)
+		}
+		counter++
+	}
+}
+func (s *Storage) ScanDiskForDownloaded(hashedPieces []byte) error {
+
+	// s.testfunc()
+
+	// return nil
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	buffer := make([]byte, s.pieceLength)
+
+	test := map[int]struct{}{}
+	bufferStart := 0
+	i := 0
+
+	counter := 0
+	for fileIdx, file := range s.files {
+		fmt.Println("curr file info", file.Size, file.Handler.Name())
+		reader := bufio.NewReader(file.Handler)
+
+		for {
+			n, err := reader.Read(buffer[bufferStart:])
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("tu sam", i, n, counter, fileIdx)
+					//return nil
+					break
+				}
+				log.Fatalf("error reading file: %s", err)
+			}
+			fmt.Println(n+bufferStart == len(buffer), n, bufferStart, n+bufferStart, len(buffer))
+			if n+bufferStart == len(buffer) || (n+bufferStart != len(buffer) && fileIdx == len(s.files)-1) {
+				expected := hashedPieces[i : i+20]
+				sum := sha1.Sum(buffer[:n+bufferStart])
+				if !bytes.Equal(sum[:], expected) {
+					fmt.Println("netacno", n, i/20, file.Handler.Name())
+					test[i/20] = struct{}{}
+				}
+				i += 20
+				bufferStart = 0
+				counter++
+				clear(buffer)
+			} else {
+				bufferStart += n
+			}
+
+		}
+	}
+	fmt.Println(test, len(test), counter, s.totalSize)
+	return nil
 }
 
 func (s *Storage) AddToBuffer(data []byte, index int64) {
